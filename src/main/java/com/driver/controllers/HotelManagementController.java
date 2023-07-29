7131585a321d2390567bcbb965d8a74eb5178b06
momgupta123 <momgupta06@gmail.com>
2023-07-29 21:54:26 +0530

    first  commit

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -12,74 +12,103 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/hotel")
 public class HotelManagementController {
 
+    private Map<String, Hotel> hotelDb = new HashMap<>();
+    private Map<Integer, Booking> bookingDb = new HashMap<>();
+    private Map<Integer, User> userDb = new HashMap<>();
+    private int bookingIdCounter = 1;
+
     @PostMapping("/add-hotel")
-    public String addHotel(@RequestBody Hotel hotel){
+    public String addHotel(@RequestBody Hotel hotel) {
+        if (hotel == null || hotel.getHotelName() == null) {
+            return "FAILURE";
+        }
 
-        //You need to add an hotel to the database
-        //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
-        //Incase somebody is trying to add the duplicate hotelName return FAILURE
-        //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
+        if (hotelDb.containsKey(hotel.getHotelName())) {
+            return "FAILURE";
+        }
 
-
-        return null;
+        hotelDb.put(hotel.getHotelName(), hotel);
+        return "SUCCESS";
     }
 
     @PostMapping("/add-user")
-    public Integer addUser(@RequestBody User user){
-
-        //You need to add a User Object to the database
-        //Assume that user will always be a valid user and return the aadharCardNo of the user
-
-       return null;
+    public Integer addUser(@RequestBody User user) {
+        int aadharCardNo = user.hashCode();
+        user.setAadharCardNo(aadharCardNo);
+        userDb.put(aadharCardNo, user);
+        return aadharCardNo;
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
-    public String getHotelWithMostFacilities(){
+    public String getHotelWithMostFacilities() {
+        if (hotelDb.isEmpty()) {
+            return "";
+        }
 
-        //Out of all the hotels we have added so far, we need to find the hotelName with most no of facilities
-        //Incase there is a tie return the lexicographically smaller hotelName
-        //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
+        Hotel hotelWithMostFacilities = hotelDb.values().stream()
+                .max(Comparator.comparingInt(hotel -> hotel.getFacilities().size()))
+                .orElse(null);
 
-        return null;
+        if (hotelWithMostFacilities == null || hotelWithMostFacilities.getFacilities().isEmpty()) {
+            return "";
+        }
+
+        List<Facility> facilities = hotelWithMostFacilities.getFacilities();
+        facilities.sort(Comparator.comparing(Facility::getFacilityName));
+        return hotelWithMostFacilities.getHotelName();
     }
 
     @PostMapping("/book-a-room")
-    public int bookARoom(@RequestBody Booking booking){
+    public int bookARoom(@RequestBody Booking booking) {
+        Hotel hotel = hotelDb.get(booking.getHotelName());
+        if (hotel == null || hotel.getAvailableRooms() < booking.getNumberOfRooms()) {
+            return -1;
+        }
 
-        //The booking object coming from postman will have all the attributes except bookingId and amountToBePaid;
-        //Have bookingId as a random UUID generated String
-        //save the booking Entity and keep the bookingId as a primary key
-        //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
-        //If there arent enough rooms available in the hotel that we are trying to book return -1 
-        //in other case return total amount paid 
-        
-        return 0;
+        String bookingId = UUID.randomUUID().toString();
+        booking.setBookingId(bookingId);
+        int totalAmountPaid = booking.getNumberOfRooms() * hotel.getPricePerNight();
+        booking.setAmountToBePaid(totalAmountPaid);
+
+        bookingDb.put(bookingIdCounter, booking);
+        bookingIdCounter++;
+
+        hotel.setAvailableRooms(hotel.getAvailableRooms() - booking.getNumberOfRooms());
+        return totalAmountPaid;
     }
-    
+
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
-    public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
-    {
-        //In this function return the bookings done by a person 
-        return 0;
+    public List<Booking> getBookings(@PathVariable("aadharCard") Integer aadharCard) {
+        return bookingDb.values().stream()
+                .filter(booking -> booking.getAadharCard() == aadharCard)
+                .collect(Collectors.toList());
     }
 
     @PutMapping("/update-facilities")
-    public Hotel updateFacilities(List<Facility> newFacilities,String hotelName){
-
-        //We are having a new facilites that a hotel is planning to bring.
-        //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
-        //return the final updated List of facilities and also update that in your hotelDb
-        //Note that newFacilities can also have duplicate facilities possible
-        return null;
+    public Hotel updateFacilities(@RequestBody List<Facility> newFacilities, String hotelName) {
+        Hotel hotel = hotelDb.get(hotelName);
+        if (hotel != null) {
+            List<Facility> existingFacilities = hotel.getFacilities();
+            for (Facility facility : newFacilities) {
+                if (!existingFacilities.contains(facility)) {
+                    existingFacilities.add(facility);
+                }
+            }
+            hotelDb.put(hotelName, hotel);
+        }
+        return hotel;
     }
-
 }
